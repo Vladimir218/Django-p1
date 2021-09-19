b3d3288079dd950658598dc347c063a4e8bcfdb7
vladimir <kvv23@tut.by>
2021-09-19 18:43:18 +0300

    Урок2 создана переменная для вывода названия title и добавлена текущая дата в панель навигации

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request,'mainapp/index.html')
+    content = {
+        'title': 'geekshop'
+    }
+    return render(request, 'mainapp/index.html', context=content)
+
+
 def products(request):
-    return render(request,'mainapp/products.html')
+    content = {
+        'title': 'geekshop'
+    }
+    return render(request, 'mainapp/products.html', context=content)
