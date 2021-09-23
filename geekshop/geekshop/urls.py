64e183eff69f63891ca1bc5d0f77ab7d1a699d14
vladimir <kvv23@tut.by>
2021-09-23 23:34:21 +0300

    Урок3 подкорректировал код под медиа и загрузил картинки через админку

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('products/', include('mainapp.urls',namespace='mainapp')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
