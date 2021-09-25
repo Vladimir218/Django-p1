146a19a2dc1222b37edb3f56e830f55f5b765666
vladimir <kvv23@tut.by>
2021-09-25 11:41:54 +0300

    Урок3 Доработан вывод картинок товаров и список категорий товаров

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import json, os
 
+from .models import ProductCategory, Product
+
 CURENT_DIR = os.path.dirname(__file__)
 
 
@@ -14,10 +16,15 @@ def index(request):
 
 
 def products(request):
-    catalog_path = os.path.join(CURENT_DIR, 'fixtures/catalog.json')
-    with open(catalog_path, 'r', encoding='utf-8') as f:
-        content = {
-            'title': 'geekshop',
-            'things': json.load(f)
-        }
+    # catalog_path = os.path.join(CURENT_DIR, 'fixtures/catalog.json')
+    # with open(catalog_path, 'r', encoding='utf-8') as f:
+    #     content = {
+    #         'title': 'geekshop',
+    #         'things': json.load(f)
+    #     }
+    title ='каталог'
+    products = Product.objects.all()
+    productsCategory = ProductCategory.objects.all()
+    content = {'title':title, 'things':products, 'categores':productsCategory}
     return render(request, 'mainapp/products.html', context=content)
+
