f6671cb3451e7bfff13aa478c5ec53df1c8b7492
vladimir <kvv23@tut.by>
2021-09-29 21:20:40 +0300

    Урок4. Исправление ошибок в разделе, связанном с регистрацией пользователя

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,49 +1,50 @@
-from django.shortcuts import render,HttpResponsePermanentRedirect
+from django.shortcuts import render,HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-
-from users.forms import UserLoginForm, UserRegisterForm
 # Create your views here.
+from users.forms import UserLoginForm,UserRegisterForm
+
 
 def login(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username =request.POST['username']
+            username = request.POST['username']
             password = request.POST['password']
-            user=auth.authenticate(username=username,password=password)
+            user = auth.authenticate(username=username,password=password)
             if user.is_active:
-                auth.login(request, user)
-                return HttpResponsePermanentRedirect(reverse('index'))
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
         else:
-            print((form.errors))
+            print(form.errors)
     else:
         form = UserLoginForm()
     context = {
-        'title':'GeekShop Авторизация',
-        'form': form
-    }
+            'title': ' Geekshop - Авторизация',
+            'form': form
+        }
     return render(request, 'users/login.html', context)
 
-def register(request):
 
-    if request.method=="POST":
-        form = UserRegisterForm(data = request.POST)
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponsePermanentRedirect(reverse('users:login'))
+            return HttpResponseRedirect(reverse('users:login'))
         else:
             print(form.errors)
     else:
         form = UserRegisterForm()
-
     context = {
-        'title':'GeekShop Регистрация',
-         'form':form
+        'title': ' Geekshop - Регистрация',
+        'form':form
+
     }
     return render(request, 'users/register.html', context)
 
+
 def logout(request):
     auth.logout(request)
-    return HttpResponsePermanentRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('index'))
