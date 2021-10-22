fa384827bdaeb3dcd6c71ad562fee42ee2b5e237
vladimir <kvv23@tut.by>
2021-10-22 22:21:47 +0300

    Урок8. Пагинация страниц товаров

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import json, os
 
 from .models import ProductCategory, Product
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 
 CURENT_DIR = os.path.dirname(__file__)
 
@@ -15,16 +16,26 @@ def index(request):
     return render(request, 'mainapp/index.html', context=content)
 
 
-def products(request,category_id = None):
+def products(request,category_id = None, page_id=1):
     # catalog_path = os.path.join(CURENT_DIR, 'fixtures/catalog.json')
     # with open(catalog_path, 'r', encoding='utf-8') as f:
     #     content = {
     #         'title': 'geekshop',
     #         'things': json.load(f)
     #     }
-    title ='каталог'
+    title ='Каталог'
     products = Product.objects.filter(category_id = category_id)  if category_id != None else Product.objects.all()
+    paginator = Paginator(products,per_page=2)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(Paginator.num_pages)
+
     productsCategory = ProductCategory.objects.all()
-    content = {'title':title, 'things':products, 'categores':productsCategory}
+    content = {'title':title,
+               'categores':productsCategory}
+    content.update({'things':products_paginator})
     return render(request, 'mainapp/products.html', context=content)
 
